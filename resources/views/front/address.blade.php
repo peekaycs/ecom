0933ad38bbbe76718d5508272ef7853cda6f1afa
Pramod Singh <prmodcs@gmail.com>
2023-04-22 10:55:57 +0530

    minor design changes

--- resources/views/front/address.blade.php
+++ resources/views/front/address.blade.php
@@ -41,7 +41,7 @@
                                                         </div>
                                                         <div class="col-md-6 col-sm-6 col-12">
                                                             <div class="mb-2">
-                                                            <input type="text" name="contact" class="form-control rounded-0" value="{{ $address->contact ?? '' }}" placeholder="Contact Number">
+                                                            <input type="number" pattern="[0-9]{10}" name="contact" class="form-control rounded-0" value="{{ $address->contact ?? '' }}" placeholder="Contact Number">
                                                             </div>				
                                                         </div>
                                                         <div class="col-md-6 col-sm-6 col-12">
@@ -130,17 +130,17 @@
                         <div class="row">
                             <div class="col-md-6 col-sm-6 col-12">
                        			<div class="mb-2">
-                                    <input type="text" name="name" class="form-control rounded-0" placeholder="Enter Your Name">
+                                    <input type="text" name="name" class="form-control rounded-0" placeholder="Enter Your Name" required>
                                 </div>				
                             </div>
                             <div class="col-md-6 col-sm-6 col-12">
                                 <div class="mb-2">
-                                 <input type="text" name="contact" class="form-control rounded-0" placeholder="Contact Number">
+                                 <input type="number" pattern="[0-9]{10}" name="contact" class="form-control rounded-0" placeholder="Contact Number" required>
                                 </div>				
                             </div>
                             <div class="col-md-6 col-sm-6 col-12">
                                 <div class="mb-2">
-                                    <input type="text" name="address" class="form-control rounded-0" placeholder="Enter address">
+                                    <input type="text" name="address" class="form-control rounded-0" placeholder="Enter address" required>
                                 </div>				
                             </div>
                             <div class="col-md-6 col-sm-6 col-12">
@@ -150,17 +150,17 @@
                             </div>
                             <div class="col-md-6 col-sm-6 col-12">
                                 <div class="mb-2">
-                                    <input type="text" name="zip" class="form-control rounded-0" placeholder="Pin Number">
+                                    <input type="number" pattern="[0-9]{6}" name="zip" class="form-control rounded-0" placeholder="Pin Number" required>
                                 </div>				
                             </div>
                             <div class="col-md-6 col-sm-6 col-12">
                                 <div class="mb-2">
-                                    <input type="text" name="city" class="form-control rounded-0" placeholder="City">
+                                    <input type="text" name="city" class="form-control rounded-0" placeholder="City" required>
                                 </div>				
                             </div>
                             <div class="col-md-6 col-sm-6 col-12">
                                 <div class="mb-2">
-                                    <input type="text" name="state" class="form-control rounded-0" placeholder="State">
+                                    <input type="text" name="state" class="form-control rounded-0" placeholder="State" required>
                                 </div>				
                             </div>
                             <div class="col-md-2 col-sm-4 col-4">
